09e149b2f84ce58ab0034f34a078213bc90ce577
VedantSonavane12 <156155836+VedantSonavane12@users.noreply.github.com>
2025-02-20 13:25:15 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // This must match your GitHub repo name exactly
+});
